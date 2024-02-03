640686f624b723bf90348d389a6d9c9fa7c8a5b3
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-03 11:51:52 -0300

    feat: input component

--- src/app/components/TripSearch/index.tsx
+++ src/app/components/TripSearch/index.tsx
@@ -1,3 +1,4 @@
+import Input from "@/components/input";
 import React from "react";
 
 const TripSearch = () => {
@@ -6,6 +7,15 @@ const TripSearch = () => {
       <h1 className="font-semibold text-2xl text-primaryDark text-center">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
+
+      <div className="flex flex-col gap-4 nt-5">
+        <Input placeholder="Onde você quer ir? " />
+
+        <div className="flex gap-4 ">
+          <Input placeholder="Data de ida" />
+          <Input placeholder="Orçamento?" />
+        </div>
+      </div>
     </div>
   );
 };
