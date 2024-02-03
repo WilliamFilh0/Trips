758ceb9cb50948d3da8a37fbe6e1d47370e54f65
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-03 11:11:43 -0300

    feat: TripsSearch Page

---
+++ src/app/components/TripSearch/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TripSearch = () => {
+  return (
+    <div className="container mx-auto ">
+      <h1 className="font-semibold text-2xl text-primaryDark text-center">
+        Encontre sua próxima <span className="text-primary">viagem!</span>
+      </h1>
+    </div>
+  );
+};
+
+export default TripSearch;
