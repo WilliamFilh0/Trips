9321d66ca94263e4d914179fffa55bb35d213812
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-04 13:01:44 -0300

    feat: icons of QuickSearch

--- src/app/components/QuickSearcyh.tsx
+++ src/app/components/QuickSearcyh.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import React from "react";
+
 const QuickSearch = () => {
   return (
     <div className="container mx-auto p-5">
@@ -7,6 +10,28 @@ const QuickSearch = () => {
           Tente pesquisar por
         </h2>
         <div className="w-full h-[1px] bg-grayLighter"></div>
+      </div>
+
+      <div className="flex w-full justify-between mt-5">
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
+          <p className="text-sn text-grayPrimary ">Hotel</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/farm-icon.png" alt="Fazenda" />
+          <p className="text-sn text-grayPrimary ">Fazenda</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/cottage-icon.png" alt="Chalé" />
+          <p className="text-sn text-grayPrimary ">Chalé</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/inn-icon.png" alt="Pousada" />
+          <p className="text-sn text-grayPrimary ">Pousada</p>
+        </div>
       </div>
     </div>
   );
