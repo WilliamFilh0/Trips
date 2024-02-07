29d688d43a0f3339b3e5b3675f8128a00157fe66
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-07 11:26:08 -0300

    feat: RecommendedTrips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import QuickSearch from "./components/QuickSearcyh";
 import TripSearch from "./components/TripSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
