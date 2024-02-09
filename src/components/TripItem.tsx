b2cb5bd9d37e97fea0f16ed17be8aab9afef2388
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-09 12:24:56 -0300

    feat: add initial trip details page and trip header

--- src/components/TripItem.tsx
+++ src/components/TripItem.tsx
@@ -1,5 +1,6 @@
 import { Trip } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import ReactCountryFlag from "react-country-flag";
 
@@ -9,28 +10,36 @@ interface TripItemProps {
 
 const TripItem = ({ trip }: TripItemProps) => {
   return (
-    <div className="flex flex-col">
-      <Image
-        src={trip.coverImage}
-        width={280}
-        height={280}
-        className="rounded-lg shadow-md"
-        alt={trip.name}
-      />
+    <Link href={`/trips/${trip.id}`}>
+      <div className="flex flex-col">
+        <div className="relative h-[280px] w-[280px]">
+          <Image
+            src={trip.coverImage}
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            className="rounded-lg shadow-md"
+            alt={trip.name}
+          />
+        </div>
 
-      <h3 className="text-primaryDark font-medium text-sm mt-2">{trip.name}</h3>
+        <h3 className="text-primaryDark font-medium text-sm mt-2">
+          {trip.name}
+        </h3>
 
-      <div className="flex items-center gap-1 my-1">
-        <ReactCountryFlag countryCode={trip.countryCode} svg />
-        <p className="text-xs text-grayPrimary">{trip.location}</p>
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg />
+          <p className="text-xs text-grayPrimary">{trip.location}</p>
+        </div>
+        <p className="text-xs text-grayPrimary">
+          <span className="text-primary font-medium">
+            R${trip.pricePerDay.toString()}
+          </span>{" "}
+          por dia
+        </p>
       </div>
-      <p className="text-xs text-grayPrimary">
-        <span className="text-primary font-medium">
-          R${trip.pricePerDay.toString()}
-        </span>{" "}
-        por dia
-      </p>
-    </div>
+    </Link>
   );
 };
 
