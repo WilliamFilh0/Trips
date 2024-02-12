a749f59bf86e1aed4436b340547235f367ba717d
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-12 11:32:56 -0300

    feat: add location description

---
+++ prisma/migrations/20240212134816_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Fusce scelerisque sem eu accumsan tristique. In pellentesque neque at neque congue placerat. Fusce fringilla viverra urna non elementum neque sollicitudin ut';
