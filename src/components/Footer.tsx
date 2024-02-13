3388e7f0d3fe7369e0b71fb030d27b01066bb188
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-13 13:04:03 -0300

    style: apply font-semibold to make text semi-bold

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="bg-walterWhite p-5 flex justify-center flex-col items-center">
       <Image src="/logo.png" width={133} height={23} alt="Full Stack Week" />
-      <p className="text-sm font-medium mt-1 text-primaryDark">
+      <p className="text-sm font-semibold mt-1 text-primaryDark">
         Todos os direitos reservados.
       </p>
     </div>
