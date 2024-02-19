68e0d341c08a3f48b1b502037d4ae9fbf6581570
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-19 09:56:47 -0300

    feat: add more date validation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -7,11 +7,13 @@ import { differenceInDays } from "date-fns";
 import { Trip } from "@prisma/client";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
+import { error } from "console";
 
 interface TripReservationProps {
+  tripId: String;
   tripStartDate: Date;
   tripEndDate: Date;
-  maxGuests: Number;
+  maxGuests: number;
   pricePerDay: number;
 }
 
@@ -26,6 +28,7 @@ const TripReservation = ({
   tripStartDate,
   tripEndDate,
   pricePerDay,
+  tripId,
 }: TripReservationProps) => {
   const {
     register,
@@ -33,10 +36,49 @@ const TripReservation = ({
     formState: { errors },
     control,
     watch,
+    setError,
   } = useForm<TripReservationForm>();
 
-  const onSubmit = (data: any) => {
-    console.log({ data });
+  const onSubmit = async (data: TripReservationForm) => {
+    const response = await fetch("http://localhost:3000/api/trips/check", {
+      method: "POST",
+      body: Buffer.from(
+        JSON.stringify({
+          startDate: data.startDate,
+          endDate: data.endDate,
+          tripId,
+        })
+      ),
+    });
+
+    const res = await response.json();
+
+    console.log({ res });
+
+    if (res?.error?.code === "TRIP_ALREADY_RESERVED") {
+      setError("startDate", {
+        type: "manual",
+        message: "Esta data já está reservada. ",
+      });
+      setError("endDate", {
+        type: "manual",
+        message: "Esta data já está reservada.",
+      });
+    }
+
+    if (res?.error?.code === "INVALID_START_DATE") {
+      setError("startDate", {
+        type: "manual",
+        message: "Data Invalida. ",
+      });
+    }
+
+    if (res?.error?.code === "INVALID_END_DATE") {
+      setError("endDate", {
+        type: "manual",
+        message: "Data Invalida. ",
+      });
+    }
   };
 
   const startDate = watch("startDate");
