8beee2b4284ac0e4bc6e07a17b544cb722fa48b5
WilliamFilh0 <williamfilho8908@gmail.com>
2024-03-01 12:35:32 -0300

    fix(api): resolve invalid export in trip reservation route

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -4,18 +4,16 @@ import { NextResponse } from "next/server";
 export async function DELETE(
   _request: Request,
   { params: { reservationId } }: { params: { reservationId: string } }
-): Promise<void | Response> {
+) {
   if (!reservationId) {
-    return new Response(JSON.stringify({ message: "Missing reservationId" }), {
-      status: 400,
-    });
+    throw new Error("Missing reservationId");
   }
 
-  await prisma.tripReservation.delete({
+  const reservation = await prisma.tripReservation.delete({
     where: {
       id: reservationId,
     },
   });
 
-  return new Response(JSON.stringify({}), { status: 200 });
+  return new NextResponse(JSON.stringify(reservation), { status: 200 });
 }
