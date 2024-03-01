b275dde9eb9400c298f991007668b05609fd81dd
WilliamFilh0 <williamfilho8908@gmail.com>
2024-02-29 22:51:16 -0300

    fix(api): Attempting to fix the error "Route 'src/app/api/trips/reservation/[reservationId]/route.ts' has an invalid export."

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -4,44 +4,18 @@ import { NextResponse } from "next/server";
 export async function DELETE(
   _request: Request,
   { params: { reservationId } }: { params: { reservationId: string } }
-): Promise<Response> {
+): Promise<void | Response> {
   if (!reservationId) {
-    return new Response(
-      JSON.stringify({
-        message: "Missing reservationId",
-      }),
-      {
-        status: 400,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    return new Response(JSON.stringify({ message: "Missing reservationId" }), {
+      status: 400,
+    });
   }
 
-  try {
-    await prisma.tripReservation.delete({
-      where: {
-        id: reservationId,
-      },
-    });
+  await prisma.tripReservation.delete({
+    where: {
+      id: reservationId,
+    },
+  });
 
-    return new Response(JSON.stringify({}), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    console.error("Error deleting reservation:", error);
-    return new Response(
-      JSON.stringify({ message: "Error deleting reservation" }),
-      {
-        status: 500,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  }
+  return new Response(JSON.stringify({}), { status: 200 });
 }
