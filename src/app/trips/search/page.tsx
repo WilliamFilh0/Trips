ba02dcf78b5f5bfc4e917957d3ba7e951288be3d
WilliamFilh0 <williamfilho8908@gmail.com>
2024-03-05 09:39:53 -0300

    fix(trips): Fix error in trips search page prerendering

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Trip } from "@prisma/client";
 import TripItem from "@/components/TripItem";
+import { Trip } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
+import { Suspense } from "react";
 
 const Trips = () => {
   const [trips, setTrips] = React.useState<Trip[]>([]);
@@ -26,25 +27,27 @@ const Trips = () => {
     };
 
     fetchTrips();
-  }, []);
+  }, [searchParams]);
 
   return (
-    <div className="container mx-auto flex flex-col items-center p-5">
-      <h1 className="text-primaryDark font-semibold text-xl">
-        Viagens Encontaradas
-      </h1>
-      <h2 className="text-grayPrimary font-medium mb-5">
-        {trips.length > 0
-          ? "Listamos as melhores viagens pra você!"
-          : "Não encontramos nada nos seus parâmetros! =("}
-      </h2>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <div className="container mx-auto flex flex-col items-center p-5">
+        <h1 className="text-primaryDark font-semibold text-xl">
+          Viagens Encontaradas
+        </h1>
+        <h2 className="text-grayPrimary font-medium mb-5">
+          {trips.length > 0
+            ? "Listamos as melhores viagens pra você!"
+            : "Não encontramos nada nos seus parâmetros! =("}
+        </h2>
 
-      <div className="flex flex-col gap-4">
-        {trips?.map((trip) => (
-          <TripItem key={trip.id} trip={trip} />
-        ))}
+        <div className="flex flex-col gap-4">
+          {trips?.map((trip) => (
+            <TripItem key={trip.id} trip={trip} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
